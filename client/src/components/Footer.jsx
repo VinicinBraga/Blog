71dd7d1f5c25d3a440f27eeef89777c941f43659
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-25 16:44:27 -0300

    Home and Single

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        © All Rights Reserved. <b>Vini Braga</b>.
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={``} alt="Img" />
+        <div className="user">
+          <img src={``} alt="Img" />
+          <div className="info">
+            <span>Vini</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Title</h1>
+        <p>Post Desc</p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
