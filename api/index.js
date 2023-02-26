39f1f3e147e387a6303cf92879460513cc8fa51c
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-26 17:38:49 -0300

    Login Controller

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
