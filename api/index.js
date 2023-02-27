edab4d03d0e3c30db5cdaadc1e652f315896f3a5
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-27 12:35:31 -0300

    AddPost and UpdatePost Controllers

--- api/index.js
+++ api/index.js
@@ -3,12 +3,30 @@ import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
