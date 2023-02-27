1588c8e41286f3adffb913e2f1bb6eb54062b8ff
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-27 11:17:07 -0300

    Single page done

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
   const q = req.query.cat
@@ -13,7 +14,14 @@ export const getPosts = (req, res) => {
 };
 
 export const getPost = (req, res) => {
-  res.json("its a post from controller");
+  const q =
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data[0]);
+  });
 };
 
 export const addPost = (req, res) => {
@@ -21,7 +29,21 @@ export const addPost = (req, res) => {
 };
 
 export const deletePost = (req, res) => {
-  res.json("its a post from controller");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can delete only your post!");
+
+      return res.json("Post has been deleted!");
+    });
+  });
 };
 
 export const updatePost = (req, res) => {
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,62 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/2777898/pexels-photo-2777898.jpeg"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"
-            alt=""
-          />
-
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
-            <span>Vinny</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi
-          ducimus. Iste doloremque voluptatem ex distinctio at maiores fuga sint
-          totam veritatis impedit eos suscipit reprehenderit aut autem odit
-          aliquid illo, vel numquam labore natus odio, excepturi consequatur.
-          Necessitatibus, vitae, aliquid velit perferendis laborum, cum quisquam
-          aperiam consequuntur magni debitis iste nam esse aut. Sequi
-          reprehenderit quibusdam dolorum temporibus, sed at quaerat ab commodi
-          neque ratione nobis sint minus debitis dolor! Magnam, soluta nesciunt
-          tempore aliquam nisi natus fugit unde earum quo eveniet! Consequuntur
-          aut blanditiis repellat nostrum quibusdam, voluptatem placeat eos
-          accusantium tempora fuga autem, ex omnis illum quae quod sequi ipsum
-          dolorum architecto dolor, id commodi exercitationem. Enim voluptatibus
-          officiis modi maiores quis eius quae qui magni autem nisi, harum
-          dignissimos. Officia totam blanditiis voluptates, veniam magnam aut
-          libero sunt consequatur ratione earum unde placeat reprehenderit
-          alias, eos ipsa nam nulla iste accusantium necessitatibus optio!
-          Eveniet sunt, aspernatur velit et corporis veritatis aperiam delectus
-          amet omnis vitae ea obcaecati maiores laudantium placeat facilis
-          officia doloremque laboriosam voluptatem officiis error illum nam
-          similique inventore. Odit, nam quam repellendus ab saepe quo ipsam
-          recusandae, repellat unde ipsa magni voluptates, et pariatur adipisci
-          sed quae nesciunt a velit fugit optio incidunt.
-        </p>
+        <h1>{post.title}</h1>
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   );
 };
